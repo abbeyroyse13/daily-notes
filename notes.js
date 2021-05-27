567250bb1a71f67bdce2b3b625636d325beb310a
abbeyroyse13 <heatherruan126@gmail.com>
2021-05-27 14:21:06 -0500

    daily notes array made

---
+++ notes.js
@@ -0,0 +1,17 @@
+const notes = [{
+        id: 1,
+        subject: "HTML/CSS",
+        date: "May 21st - 26th, 2021",
+        feeling: "I'm feeling good about my knowledge so far.",
+        timeSpent: 7200
+    },
+    {
+        id: 2,
+        subject: "Javascript",
+        date: "May 27th, 2021",
+        feeling: "I'm feeling a bit shaky but also pretty good at this point.",
+        timeSpent: 1440
+    }
+]
+
+console.log(notes);
